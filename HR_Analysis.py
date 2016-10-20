ca5d2ab87950098a203cc1f7deee006ea3851049
GadiGar <gadi.gar@gmail.com>
2016-10-20 23:29:53 +0300

    Cleaning
    Removed the TKINTER old code and applied some PyLint suggested
    modifications (cosmetic).

--- HR_Analysis.py
+++ HR_Analysis.py
@@ -1,42 +1,40 @@
+"""
+a python script analyzing garmin's .tcx files.
+eventually I want to show trends in heart-rate vs. pace.
+"""
+
 from lxml import etree
 import matplotlib.pyplot as plt
-# import tkinter
 
-class garmin:
+class Garmin:
+    """ a class for dealing with garmin's tcx files. """
 
-    def get_file(self, filename='activity_1321393768.tcx'):
+    def __init__(self, filename='activity_1321393768.tcx'):
         self.filename = filename
-        # root = tkinter.Tk()
-        # root.withdraw() # we don't want a full GUI, so keep the root window from appearing
-        # root.update()
-        # self.filename = tkinter.tkfiledialog.askopenfilename() # show an "Open" dialog box and return the path to the selected file
 
     def parse_file(self):
         tree = etree.parse(self.filename)
-        root = tree.getroot()
         xmlstr = '{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}'
         points = tree.findall('//'+xmlstr+'Trackpoint')
         self.distance = [r.find(xmlstr+'DistanceMeters').text for r in points]
-        self.hr = [r.find(xmlstr+'HeartRateBpm')[0].text for r in points]
+        self.heart_rate = [r.find(xmlstr+'HeartRateBpm')[0].text for r in points]
         self.speed = [r.find(xmlstr+'Extensions')[0][0].text for r in points]
         self.pace = [float(curr)*3600/1000 for curr in self.speed]
-        self.time = range(0,len(self.distance))
+        self.time = range(0, len(self.distance))
 
     def plot_data(self):
         plt.figure(1)
-        plt.plot(self.time,self.hr)
+        plt.plot(self.time, self.heart_rate)
         plt.figure(2)
-        plt.plot(self.time,self.speed)
+        plt.plot(self.time, self.speed)
         plt.figure(3)
-        plt.plot(self.hr,self.pace)
+        plt.plot(self.heart_rate, self.pace)
         plt.show()
 
 if __name__ == '__main__':
-    myData = garmin()
-    print("Getting file...")
-    myData.get_file()
-    print("Parsing file...")
-    myData.parse_file()
-    print("Plotting data...")
-    myData.plot_data()
-    # myData.plotData()
+    my_data = Garmin()
+    print "Getting file..."
+    print "Parsing file..."
+    my_data.parse_file()
+    print "Plotting data..."
+    my_data.plot_data()
